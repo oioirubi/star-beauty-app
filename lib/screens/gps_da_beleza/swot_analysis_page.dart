2dde84c0a129c287b6b470f8c1b9e5c72aa1d213
Logout <matheus.bertos2@gmail.com>
2025-01-15 23:24:44 -0300

    Update swot_analysis_page.dart (#6)

--- lib/screens/gps_da_beleza/swot_analysis_page.dart
+++ lib/screens/gps_da_beleza/swot_analysis_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:star_beauty_app/components/custom_container.dart';
 
@@ -9,16 +11,27 @@ class SwotAnalysisPage extends StatefulWidget {
 }
 
 class _SwotAnalysisPageState extends State<SwotAnalysisPage> {
-  final List<String> forces = ['Tarefa 1', 'Tarefa 2'];
-  final List<String> weaknesses = ['Tarefa 3'];
-  final List<String> opportunities = [];
-  final List<String> threats = [];
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<String> forces = ['Tarefa 1', 'Tarefa 2'];
+  List<String> weaknesses = ['Tarefa 3'];
+  List<String> opportunities = [];
+  List<String> threats = [];
 
   String? activeEditingCard;
   String? activeEditingColumn;
   bool isAddingCard = false;
 
   final FocusNode focusNode = FocusNode();
+
+  late Future _futureData;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _futureData = _loadData();
+  }
 
   /// Move um cartão entre colunas
   void onItemDropped(
@@ -29,6 +42,7 @@ class _SwotAnalysisPageState extends State<SwotAnalysisPage> {
       }
       targetBoard.add(data); // Adiciona à nova coluna
     });
+    _saveData();
   }
 
   /// Adiciona um novo cartão
@@ -39,6 +53,7 @@ class _SwotAnalysisPageState extends State<SwotAnalysisPage> {
       targetBoard.add('');
       activeEditingCard = '';
     });
+    _saveData();
   }
 
   /// Cancela um novo cartão
@@ -61,6 +76,7 @@ class _SwotAnalysisPageState extends State<SwotAnalysisPage> {
       activeEditingCard = null;
       isAddingCard = false;
     });
+    _saveData();
   }
 
   /// Salva ou descarta com clique fora
@@ -80,36 +96,46 @@ class _SwotAnalysisPageState extends State<SwotAnalysisPage> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: handleOutsideClick,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Row(
+    return FutureBuilder(
+      future: _futureData,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container();
+        }
+        return GestureDetector(
+          onTap: handleOutsideClick,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
                 children: [
-                  Expanded(child: _buildBoard('Forças', forces, Colors.blue)),
-                  const SizedBox(width: 16),
-                  Expanded(
-                      child:
-                          _buildBoard('Fraquezas', weaknesses, Colors.orange)),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: _buildBoard('Forças', forces, Colors.blue)),
+                      const SizedBox(width: 16),
+                      Expanded(
+                          child: _buildBoard(
+                              'Fraquezas', weaknesses, Colors.orange)),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: _buildBoard(
+                              'Oportunidades', opportunities, Colors.green)),
+                      const SizedBox(width: 16),
+                      Expanded(
+                          child: _buildBoard('Ameaças', threats, Colors.red)),
+                    ],
+                  ),
                 ],
               ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                      child: _buildBoard(
-                          'Oportunidades', opportunities, Colors.green)),
-                  const SizedBox(width: 16),
-                  Expanded(child: _buildBoard('Ameaças', threats, Colors.red)),
-                ],
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
@@ -237,5 +263,72 @@ class _SwotAnalysisPageState extends State<SwotAnalysisPage> {
         ),
       ),
     );
+  }
+
+  Future<void> _loadData() async {
+    try {
+      //verificar se o usuário é autenticado
+      final user = _auth.currentUser;
+      if (user == null) {
+        throw Exception(
+            "Usuário não autenticado"); //caso não, throw uma exception
+      }
+      final doc = await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('swot')
+          .doc('current')
+          .get();
+      if (doc.exists) {
+        final data = doc.data();
+        setState(() {
+          forces = getStringList(data?['forces']);
+          weaknesses = getStringList(data?['weaknesses']);
+          opportunities = getStringList(data?['opportunities']);
+          threats = getStringList(data?['threats']);
+        });
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('swot carregado com sucesso!')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao carregar o swot: $e')),
+      );
+    }
+  }
+
+  List<String> getStringList(dynamic data) {
+    if (data == null) return [];
+    if (data is! List) return [];
+    return data.map((e) => e?.toString() ?? '').toList();
+  }
+
+  Future<void> _saveData() async {
+    try {
+      final user = _auth.currentUser;
+      if (user == null) {
+        throw Exception(
+            "Usuário não autenticado"); //caso não, throw uma exception
+      }
+      await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('swot')
+          .doc('current')
+          .set({
+        "forces": forces,
+        "weaknesses": weaknesses,
+        "opportunities": opportunities,
+        "threats": threats
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('swot salvo com sucesso!')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao salvar swot: $e')),
+      );
+    }
   }
 }
